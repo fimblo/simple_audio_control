4e4664c698ce4e857d3d526a66f66151656c939e
Mattias Jansson <fimblo@yanson.org>
2018-10-05 21:27:59 +0900

    added usage

--- audio.sh
+++ audio.sh
@@ -32,6 +32,43 @@ inc=$(($max_volume / 20))
 # --------------------------------------------------
 # FUNCTIONS
 
+# short usage
+usage-short() {
+  cat<<-EOF
+	audio.sh, vup, vdown, vmute, vstate
+	
+	Usage: audio.sh [-+ma]+ [=|h]
+	
+	Run audio.sh without arguments and get current sink volume and mute
+	state.
+	
+	Run audio.sh with an arbritrary number of commands, each command
+	represented by a symbol:
+	  -  Turn the volume down by 5%
+	  +  Turn the volume up by 5%
+	  m  Toggle mute state
+	  a  Show source, sink, sink volume and sink mute state
+	  =  Show i3bar-friendly volume and mute state
+	
+	Examples:
+	  audio.sh         Show volume and mute state
+	  audio.sh m       Toggle mute state
+	  audio.sh +++     Turn volume up 15%
+	  audio.sh m----   Toggle mute, then turn volume down 20%
+	  audio.sh a       Show all info
+	  audio.sh h       This usage text
+	
+	Aliases
+	  vup is an alias for 'audio.sh +'
+	  vdown is an alias for 'audio.sh -'
+	  vmute is an alias for 'audio.sh m'
+	  vstate is an alias for 'audio.sh a'
+	EOF
+  exit 0
+}
+
+
+
 # Returns volume 0-100
 getratio() {
   ratio=$(( 1 + ( volstate * 100 ) / max_volume ))
@@ -106,6 +143,7 @@ esac
 
 
 # If this script is called as audio.sh
+[[ "$1" == '-h' || "$1" == '--help' ]] && usage-short
 set -- $(echo "$@" | perl -pe 's// /g') # expand all args so there are
                                         # whitespaces between each char
 case "$1" in
@@ -113,8 +151,9 @@ case "$1" in
   '+' ) up          ; shift ; $0 $@ ;;
   'm' ) toggle_mute ; shift ; $0 $@ ;;
   'a' ) getall      ; shift ; $0 $@ ;;
-  '=' ) print_bar ;;
-  *   ) getstate  ;;
+  '=' ) print_bar   ;;
+  'h' ) usage-short ;;
+  *   ) getstate    ;;
 esac
 
 exit 0
